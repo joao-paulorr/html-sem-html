13e1a8aa9ecdf3d03be8bc0061920f8613a70ad1
João P <132589767+joao-paulorr@users.noreply.github.com>
2023-06-13 17:27:41 +0000

    k

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function CreateElement() {
-    let pai = document.body
+let pai = document.body
+function jesus() {
     let filho = document.createElement('h1')
+    filho.textContent = 'Gato'
+    foto.src = 'pau.png'
     pai.appendChild(filho)
-    filho.textContent = "titulo"
 }
+
+jesus()
+
+function foto() {
+    let foto = document.createElement('img')
+    foto.src = 'gato.jpg'
+    foto.style.width = '500px'
+    foto.style.height = '400px'
+    pai.appendChild(foto)
+}
+
+
+let botao = document.createElement('button')
+botao.textContent = 'Enviar'
+pai.appendChild(botao)
+
+botao.onclick = function botao(){foto()}
